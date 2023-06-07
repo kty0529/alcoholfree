0dc9cdec1ed9dd4b7987163b1c429447c2f4e5c2
Tae Young, Kim (casper) <kty0529@gmail.com>
2023-06-07 22:59:41 +0900

    chore: 푸터에 저장소 링크 연결

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,8 +6,14 @@ const randomIcon = iconList[Math.floor(Math.random() * iconList.length)];
 function Footer() {
   return (
     <footer className="footer">
-      <span className="text">Alcohol Free ~</span>
-      <span className="icon">{randomIcon}</span>
+      <a
+        href="https://github.com/kty0529/alcoholfree"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <span className="text">Alcohol Free ~</span>
+        <span className="icon">{randomIcon}</span>
+      </a>
     </footer>
   );
 }
