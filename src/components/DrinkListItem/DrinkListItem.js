bf4607fd722377c6dcc515bc085766be55f94778
Tae Young, Kim (casper) <kty0529@gmail.com>
2023-06-12 15:25:25 +0900

    feat: 노션 API 연동

--- src/components/DrinkListItem/DrinkListItem.js
+++ src/components/DrinkListItem/DrinkListItem.js
@@ -1,38 +1,35 @@
+// import { Link } from "react-router-dom";
 import { Link } from "react-router-dom";
 import "./DrinkListItem.scss";
 
+
+
 function DrinkListItem(props) {
-  const NumberFormat = new Intl.NumberFormat();
+  // const NumberFormat = new Intl.NumberFormat();
+
+  const DATA = props.properties;
+
+  const uid = DATA.ID.unique_id.prefix + DATA.ID.unique_id.number;
+  const thumbnail = DATA.Picture.files[0]?.file.url;
+  const proof = DATA.Proof.number;
+  const name = DATA.Name.title[0]?.plain_text;
+  const name_en = DATA.Name_en.rich_text[0]?.plain_text;
+  const category = DATA.Category.select.name;
 
   return (
-    <Link to={`./${props.name.kr}`}>
-      <div className="item">
-        <div className="thumbnail">
-          {props.thumbnail && <img src={require(`../../assets/images/${props.thumbnail}`)} alt="썸네일 이미지" />}
-          {props.proof && <span className="proof">{props.proof}%</span>}
+    <Link to={uid} className="item">
+      <div className="thumbnail">
+        {thumbnail && <img src={thumbnail} alt="썸네일 이미지" />}
+        {proof && <span className="proof">{proof}%</span>}
+      </div>
+
+      <div className="content">
+        <div className="title">
+          {name}
+          {name_en && <small>{name_en}</small>}
         </div>
 
-        <div className="content">
-          <div className="title">
-            {props.name.kr}
-            {props.name.en && <small>{props.name.en}</small>}
-          </div>
-
-          <div className="category">{props.category}</div>
-
-          <div className="price">
-            {
-              props.price && props.price.map((store, index) => {
-                return index === 0 && (
-                  <div className="block" key={index}>
-                    <span className="store">{store.store}</span>
-                    <span className="amount">{NumberFormat.format(store.amount)}원</span>
-                  </div>
-                )
-              })
-            }
-          </div>
-        </div>
+        <div className="category">{category}</div>
       </div>
     </Link>
   )
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,27 +1,60 @@
+import { useEffect, useState } from "react";
+
 import DrinkListItem from "../../components/DrinkListItem/DrinkListItem";
-import DrinkData from "../../data/DrinkData.json";
+// import DrinkData from "../../data/DrinkData.json";
 
 import "./Main.scss";
 
 function Main() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [item, setItem] = useState([]);
+
+  useEffect(() => {
+    console.log("fetchDrinkList");
+
+    fetch(process.env.REACT_APP_NOTION_API)
+      .then((res) => res.json())
+      .then((res) => {
+        setIsLoaded(true);
+        setItem(res.results);
+      }, (error) => {
+        setIsLoaded(true);
+        setError(error);
+      });
+  }, []);
+
+  const statusClass = isLoaded && error === null ? "done" : "loading";
+
   return (
-      <main className="main">
-        <div className="lists">
-          <ul>
-            {
-              DrinkData.map((item, index) => {
-                return (
-                  <li key={index}>
-                    <DrinkListItem
-                      {...item} />
-                  </li>
-                )
-              })
-            }
-          </ul>
-        </div>
-      </main>
-  );
+    <main className={`main ${statusClass}`}>
+      {
+        ! isLoaded || error
+        ? (
+          <div className="message">
+            <span className="icon material-icons-outlined">hourglass_empty</span>
+            <div className="text">로딩이 계속되면 페이지를 새로고침 해주세요.</div>
+          </div>
+          )
+        : (
+          <div className="lists">
+            <ul>
+              {
+                item.map((drink, index) => {
+                  return (
+                    <li key={index}>
+                      <DrinkListItem
+                        {...drink} />
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
+        )
+      }
+    </main>
+  )
 }
 
 export default Main;
